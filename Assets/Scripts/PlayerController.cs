2c372e6cd4d29913832ff13f069cf8e7fdc6d89d
Digital Ape <digitalapemedia@gmail.com>
2018-01-23 22:58:04 +0000

    initial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+
+	public float m_speed = 10.0f;
+	public float j_amount = 20.0f;
+
+	public Vector3 boundary;
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float moveX = Input.GetAxis ("Horizontal");
+
+		Vector3 movement = new Vector3 (moveX, 0.0f, 0.0f);
+		rb.AddForce (movement * m_speed);
+
+		if( Input.GetKeyDown(KeyCode.Space))
+			{
+				rb.AddForce(Vector3.up * j_amount);
+			}
+
+
+	}
+}
---
+++ Assets/Scripts/RandomObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomObject : MonoBehaviour 
+{
+	public GameObject[] prefabObjects;
+	public Transform[] spawnPositions;
+
+
+
+	void Start()
+	{
+		Instantiate (prefabObjects [0], spawnPositions[0].position,transform.rotation);
+		Instantiate (prefabObjects [1], spawnPositions[1].position,transform.rotation);	
+		Instantiate (prefabObjects [0], spawnPositions[2].position,transform.rotation);	
+		Instantiate (prefabObjects [1], spawnPositions[3].position,transform.rotation);	
+	}
+
+	void Update()
+	{
+		
+	}
+
+}
